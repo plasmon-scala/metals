d10c2c4ecc76fbd709588fff6dc9c980242e305e
Alexandre Archambault <alexandre.archambault@gmail.com>
2025-08-24 22:47:09 +0200

    Allow to compile with interactive compiler

---
+++ mtags/src/main/scala-2/scala/meta/internal/pc/CompileProvider.scala
@@ -0,0 +1,52 @@
+package scala.meta.internal.pc
+
+import scala.meta.pc.CompileResult
+import scala.meta.pc.VirtualFileParams
+import java.{util => ju}
+import scala.jdk.CollectionConverters._
+
+class CompileProvider(val compiler: MetalsGlobal, params: VirtualFileParams) {
+  import compiler._
+
+  def compile(): CompileProvider.Result = {
+    val unit = addCompilationUnit(
+      code = params.text(),
+      filename = params.uri().toString(),
+      cursor = None
+    )
+    typeCheck(unit)
+
+    val diagnostics = compiler.reporter
+      .asInstanceOf[scala.tools.nsc.reporters.StoreReporter]
+      .infos
+      .iterator
+      .map { info =>
+        new StringBuilder()
+          .append(info.pos.source.file.path)
+          .append(":")
+          .append(info.pos.column)
+          .append(" ")
+          .append(info.msg)
+          .append("\n")
+          .append(info.pos.lineContent)
+          .append("\n")
+          .append(info.pos.lineCaret)
+          .append("\n")
+          .toString
+      }
+      .filterNot(_.contains("_CURSOR_"))
+      .toList
+
+    CompileProvider.Result(diagnostics, unit.body.toString)
+  }
+
+}
+
+object CompileProvider {
+  final case class Result(
+      scalaDiagnostics: List[String],
+      fullTree: String
+  ) extends CompileResult {
+    def diagnostics(): ju.List[String] = scalaDiagnostics.asJava
+  }
+}
--- mtags/src/main/scala-2/scala/meta/internal/pc/ScalaPresentationCompiler.scala
+++ mtags/src/main/scala-2/scala/meta/internal/pc/ScalaPresentationCompiler.scala
@@ -27,6 +27,7 @@ import scala.meta.internal.mtags.BuildInfo
 import scala.meta.internal.mtags.MtagsEnrichments._
 import scala.meta.pc.AutoImportsResult
 import scala.meta.pc.CodeActionId
+import scala.meta.pc.CompileResult
 import scala.meta.pc.CompletionItemPriority
 import scala.meta.pc.DefinitionResult
 import scala.meta.pc.DisplayableException
@@ -447,6 +448,17 @@ case class ScalaPresentationCompiler(
         params,
         Some(name)
       ).rename().asJava
+    }(params.toQueryContext)
+
+  override def compile(
+      params: VirtualFileParams
+  ): CompletableFuture[CompileResult] =
+    compilerAccess.withNonInterruptableCompiler[CompileResult](
+      CompileProvider.Result(Nil, ""),
+      params.token
+    ) { pc =>
+      val compiler = pc.compiler()
+      new CompileProvider(compiler, params).compile()
     }(params.toQueryContext)
 
   override def hover(
