ff5a6a5ac088748b7e3a8c842ebc67f159a05ab6
Alex Archambault <alexandre.archambault@gmail.com>
2025-08-24 22:47:11 +0200

    Update bsp4j to 2.2.0-M3

--- project/V.scala
+++ project/V.scala
@@ -16,7 +16,7 @@ object V {
   val betterMonadicFor = "0.3.1"
   val bloop = "2.0.10"
   val bloopConfig = "2.3.2"
-  val bsp = "2.2.0-M2"
+  val bsp = "2.2.0-M3"
   val coursier = "2.1.24"
   // changing coursier interfaces version may be not binary compatible.
   // After each update of coursier interfaces, remember to bump the version in dotty repository.
